1ce0e8145c1e94001ac8eacd0763a048ca4d345f
Shubham Kumar <meetshubhamku@gmail.com>
2021-05-24 22:31:26 +0530

    modified database. Added course_category table

--- database/e_learning.sql
+++ database/e_learning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2021 at 09:33 AM
+-- Generation Time: May 24, 2021 at 06:59 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `e_learning`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_category`
+--
+
+CREATE TABLE `course_category` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(30) NOT NULL,
+  `course_description` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course_category`
+--
+
+INSERT INTO `course_category` (`course_id`, `course_name`, `course_description`) VALUES
+(1, 'PYTHON', 'Video series on python');
 
 -- --------------------------------------------------------
 
@@ -49,6 +68,12 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `use
 --
 
 --
+-- Indexes for table `course_category`
+--
+ALTER TABLE `course_category`
+  ADD PRIMARY KEY (`course_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -58,6 +83,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `course_category`
+--
+ALTER TABLE `course_category`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
